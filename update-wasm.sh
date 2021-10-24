387880eb9e332d72b75a680afa3f3bdbcb9f7650
Georgi Gerganov <ggerganov@gmail.com>
2021-10-24 16:08:32 +0300

    Deployment fixes

---
+++ update-wasm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp build-em/bin/t2d-explorer.data ./public/
+cp build-em/bin/t2d-explorer.js ./public/
+cp build-em/bin/t2d-explorer.wasm ./public/
+
+cp build-em/bin/t2d-explorer-extra/background-0.png ./public/images/
+cp build-em/bin/t2d-explorer-extra/gghelpers.js ./public/js/
+cp build-em/bin/t2d-explorer-extra/index.html ./public/
+cp build-em/bin/t2d-explorer-extra/style.css ./public/
