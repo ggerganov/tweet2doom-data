c8d7267cb146fbcfe7bc1a3272b9dd3e7e855fa5
Georgi Gerganov <ggerganov@gmail.com>
2021-10-24 13:28:47 +0000

    Migrate service.sh

---
+++ service.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+wd=$(dirname $0)
+cd $wd/
+wd=$(pwd)
+
+while true ; do
+    sleep 10
+
+    echo "Doing update `date` ..."
+
+    cd $wd/public/
+
+    git pull --rebase
+
+    cd $wd
+
+    ./sync.sh > log-sync.txt
+
+    cd $wd/public/
+
+    num_new=$(git ls-files --others --exclude-standard data/nodes | wc -l)
+    if [ "$num_new" -eq 0 ] ; then
+        echo "Nothing to update"
+        continue;
+    fi
+
+    cd $wd
+
+    ./generate-graph.sh
+
+    cd $wd/public/
+
+    git add data/*
+    git add js/graph.js
+    git add json/*
+
+    git commit -m "New states added [`date`]"
+    git push
+done
