bd181e1e857d9741b2ec5f8e33fb4495a112bd55
Georgi Gerganov <ggerganov@gmail.com>
2021-10-26 19:37:30 +0300

    explorer : fix input handling + loading display

--- update-wasm.sh
+++ update-wasm.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-cp build-em/bin/t2d-explorer.data ./public/
-cp build-em/bin/t2d-explorer.js ./public/
-cp build-em/bin/t2d-explorer.wasm ./public/
+wd=$(dirname $0)
+cd $wd
+wd=$(pwd)
 
-cp build-em/bin/t2d-explorer-extra/background-0.png ./public/images/
-cp build-em/bin/t2d-explorer-extra/gghelpers.js ./public/js/
-cp build-em/bin/t2d-explorer-extra/index.html ./public/
-cp build-em/bin/t2d-explorer-extra/style.css ./public/
+cd $wd
+
+cp -v build-em/bin/t2d-explorer.data ./public/
+cp -v build-em/bin/t2d-explorer.js   ./public/
+cp -v build-em/bin/t2d-explorer.wasm ./public/
+
+cp -v build-em/bin/t2d-explorer-extra/style.css        ./public/
+cp -v build-em/bin/t2d-explorer-extra/index.html       ./public/
+cp -v build-em/bin/t2d-explorer-extra/gghelpers.js     ./public/js/
+cp -v build-em/bin/t2d-explorer-extra/background-0.png ./public/images/
